7df736da2cc3d7260e19a9d025477b3c7ec7fbb9
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-10-07 10:21:14 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,54 @@
+<?php
+echo "<h1>Daftar Menu</h1>";
+$ayamGoreng = "Ayam Goreng";
+$ayamSayur = "Ayam Sayur";
+$tempeGoreng = "Tempe Goreng";
+$tahuGoreng = "Tahu Goreng";
+$soto = "Soto";
+$nasiPutih = "Nasi Putih";
+$esTeh = "Es Teh";
+$esTebu = "Es Tebu";
+
+echo "<table>
+		<tr>
+			<td><li>$ayamGoreng</li></td>
+			<td>=</td>
+			<td>Rp. 13000</td> 
+		</tr>
+		<tr>
+			<td><li>$ayamSayur</li></td>
+			<td>=</td>
+			<td>Rp. 13000</td> 
+		</tr>
+		<tr>
+			<td><li>$tempeGoreng</li></td>
+			<td>=</td>
+			<td>Rp. 1000</td> 
+		</tr>
+		<tr>
+			<td><li>$tahuGoreng</li></td>
+			<td>=</td>
+			<td>Rp. 1000</td> 
+		</tr>
+		<tr>
+			<td><li>$soto</li></td>
+			<td>=</td>
+			<td>Rp. 3000</td> 
+		</tr>
+		<tr>
+			<td><li>$nasiPutih</li></td>
+			<td>=</td>
+			<td>Rp. 5000</td> 
+		</tr>
+		<tr>
+			<td><li>$esTeh</li></td>
+			<td>=</td>
+			<td>Rp. 4000</td> 
+		</tr>
+		<tr>
+			<td><li>$esTebu</li></td>
+			<td>=</td>
+			<td>Rp. 5000</td> 
+		</tr>
+	</table>"
+?>
